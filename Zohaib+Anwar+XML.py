a0867ecc54924a3f093befc564a2e68dac9097ed
zanwar369 <zanwar369@gmail.com>
2017-07-20 02:07:28 -0400

    Added and Edited Files

--- Zohaib+Anwar+XML.py
+++ Zohaib+Anwar+XML.py
@@ -1,27 +1,11 @@
-
-# coding: utf-8
-
-# # Introduction #
-
-# In this project I download the New York xml file from OpenStreetMaps.org, parse it to get data from nodes and ways, and then clean the street names to ensure they have the same formats. The only real problem is that it seems some street names are completely wrong and would need to either be deleted or further delved into.
-
-# In[1]:
-
-from bs4 import BeautifulSoup                                  #Importing Relevant Packages
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity="all"
 import zipfile as zf
 import requests, io
 import shutil
 import html5lib
-from plotly.offline import iplot, iplot_mpl 
 import plotly.graph_objs as go
-get_ipython().magic('matplotlib inline')
 import bz2
 import xml.etree.cElementTree as et
 from collections import defaultdict
@@ -32,19 +16,12 @@ import codecs
 import cerberus
 import sqlite3
 from scipy import stats
-from IPython.display import display
-
-
-# In[22]:
 
 url="https://s3.amazonaws.com/metro-extracts.mapzen.com/new-york_new-york.osm.bz2"
-r=requests.get(url,timeout=1)                                                       #Downloading the dataset to the notebook
-
-
-# In[23]:
+r=requests.get(url,timeout=1)
 
 def count_tags(filename):
-        tags=defaultdict()                                                          #Checking the count of each type of tag
+        tags=defaultdict()
         for event, element in et.iterparse(filename,events=("start",)):
             if element.tag not in tags.keys():
                 tags[element.tag]=1
@@ -54,16 +31,8 @@ def count_tags(filename):
 
 with bz2.BZ2File(io.BytesIO(r.content)) as xml:
     tags=count_tags(xml)
-
-
-# As can bee seen there are 11.5 million nodes, and 1.8 million ways. A lot of this code will obviously take a while to run.
-
-# In[26]:
-
+    
 tags
-
-
-# In[28]:
 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')                    #Using Regex to create patterns to recognize potentional
@@ -94,21 +63,14 @@ def process_map_keys(filename):
 
 with bz2.BZ2File(io.BytesIO(r.content)) as xml:
     keys=process_map_keys(xml)
-
-
-# Most of the problems with the street names is that they seem to be in different formats as seen below
-
-# In[29]:
-
 keys
 
-
-# After doing some preliminary investigation. I begin to parse the file for street names that need fixing.
-
-# In[34]:
-
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE) #Regex expression to search for street names at the end of terms.
-expected = ["Airport","Alley","Avenue", "Boulevard","Bridge","Building","Circle",             "Close","Court","Concourse","Commerce", "Common","Commons","Crescent","Cross","Drive",            "Driveway","Expressway","Highway","Lane","Loop","Park","Parkway","Path",            "Place","Plaza""Ridge","Road","Route","Run","Slip","Square","Street","Suite",            "Terrace","Trace","Trail","Thruway","Turnpike","Walk","Walkway","Way"]
+expected = ["Airport","Alley","Avenue", "Boulevard","Bridge","Building","Circle", \
+            "Close","Court","Concourse","Commerce", "Common","Commons","Crescent","Cross","Drive",\
+            "Driveway","Expressway","Highway","Lane","Loop","Park","Parkway","Path",\
+            "Place","Plaza""Ridge","Road","Route","Run","Slip","Square","Street","Suite",\
+            "Terrace","Trace","Trail","Thruway","Turnpike","Walk","Walkway","Way"]
 # List of expected names of streets.
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")                      #Function to determine if element has a street name.
@@ -133,16 +95,8 @@ def audit(osm_file):
 with bz2.BZ2File(io.BytesIO(r.content)) as osm_file:
     street_type=audit(osm_file)
 
-
-# In[ ]:
-
 with open('C://Users/Zohaib/Desktop/Lectures/Udacity/Streets.txt','w') as f:     #Print the street types to a file called 
     pprint.pprint(street_type,f)                                                 #Streets.txt to create the mapping dictionary.
-
-
-# After parsing the file for odd street types, a mapping dictionary is created below to fix the street tyes.
-
-# In[35]:
 
 mapping = { "Americas\n":"Americas",
             "Ave.":"Avenue",
@@ -240,12 +194,7 @@ def update_name(name, mapping):
 for st_type, ways in street_type.items():
         for name in ways:
             better_name = update_name(name, mapping)
-
-
-# Finally, after creating this function, I can now parse the XML document completely and output the relevant lines into csv files.
-
-# In[36]:
-
+            
 NODES_PATH = "nodes.csv"
 NODE_TAGS_PATH = "nodes_tags.csv"
 WAYS_PATH = "ways.csv"
@@ -336,9 +285,6 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
     elif element.tag == 'way':                                  #Returns the relevant lists for the function.
         return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
 
-
-# In[37]:
-
 def get_element(osm_file, tags=('node', 'way', 'relation')):
     context = et.iterparse(osm_file, events=('start', 'end'))
     _, root = next(context)
@@ -367,12 +313,13 @@ class UnicodeDictWriter(csv.DictWriter, object):
         for row in rows:
             self.writerow(row)
 
-
-# In[20]:
-
 def process_map(file_in, validate):
 
-    with codecs.open(NODES_PATH, 'w', encoding='utf-8') as nodes_file,        codecs.open(NODE_TAGS_PATH,'w', encoding='utf-8') as nodes_tags_file,        codecs.open(WAYS_PATH, 'w', encoding='utf-8') as ways_file,        codecs.open(WAY_NODES_PATH, 'w', encoding='utf-8') as way_nodes_file,        codecs.open(WAY_TAGS_PATH, 'w', encoding='utf-8') as way_tags_file:
+    with codecs.open(NODES_PATH, 'w', encoding='utf-8') as nodes_file,\
+        codecs.open(NODE_TAGS_PATH,'w', encoding='utf-8') as nodes_tags_file,\
+        codecs.open(WAYS_PATH, 'w', encoding='utf-8') as ways_file,\
+        codecs.open(WAY_NODES_PATH, 'w', encoding='utf-8') as way_nodes_file,\
+        codecs.open(WAY_TAGS_PATH, 'w', encoding='utf-8') as way_tags_file:
 
         nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
         node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
@@ -406,34 +353,17 @@ def process_map(file_in, validate):
 with bz2.BZ2File(io.BytesIO(r.content)) as osm_file:
     process_map(osm_file, validate=True)
 
-
-# After this step the file has been iteratively parsed, and the output has been saved to csv files. Thereafter I upload the files to SQLite database called "New_York". Now I access it to run a general query for information.
-
-# In[3]:
-
 db=sqlite3.connect("C://Users/Zohaib/Desktop/Lectures/Udacity/sqlite_windows/New_York.db")
 cursor=db.cursor()   #Connects to the database with the data from the XML file.
-
-
-# In[38]:
 
 cursor.execute("select user,count(*) from nodes group by user;")
 number_of_posts=[x[1] for x in cursor.fetchall()];
 
-
-# In[39]:
-
 cursor.execute("select user,count(*) from ways group by user;")
 number2=[x[1] for x in cursor.fetchall()]
-number_of_posts=number_of_posts+number2;                            #Returns count of posts by each user.
-
-
-# In[43]:
+number_of_posts=number_of_posts+number2;                
 
 stats.mstats.mquantiles(number_of_posts,prob=[.25,.5,.75])
-
-
-# In[48]:
 
 posts_reasonable=[]
 for i in number_of_posts:
@@ -448,45 +378,20 @@ plt.ylabel("Count",fontsize=15)
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15);
 
-
-# As can be seen above most users post between 1 to about 5 times. Then the number of posts by users decreases steadily. This is to be expected from OpenStreetMaps, as many different users are posting.
-
-# Furthermore, here is a general overview of the data set as seen through some general queries to confirm what was seen before:
-
-# In[55]:
-
-cursor.execute("select sum1+sum2+sum3+sum4+sum5 from (select count(*) as sum1 from nodes) as sub1,                (select count(*) as sum2 from node_tags) as sub2,                (select count(*) as sum3 from ways) as sub3,                (select count(*) as sum4 from way_tags) as sub4,                (select count(*) as sum5 from way_nodes) as sub5;")
+cursor.execute("select sum1+sum2+sum3+sum4+sum5 from (select count(*) as sum1 from nodes) as sub1,\
+                (select count(*) as sum2 from node_tags) as sub2,\
+                (select count(*) as sum3 from ways) as sub3,\
+                (select count(*) as sum4 from way_tags) as sub4,\
+                (select count(*) as sum5 from way_nodes) as sub5;")
 cursor.fetchall()[0][0]
 
-
-# This means there was a total of 37,506,549 lines of code taken from the xml file for just ways and nodes!
-
-# In[69]:
-
-cursor.execute("select sum1+sum2 from (select count(*) as sum1 from nodes) as sub1,                (select count(*) as sum2 from ways) as sub2")
+cursor.execute("select sum1+sum2 from (select count(*) as sum1 from nodes) as sub1,\
+                (select count(*) as sum2 from ways) as sub2")
 cursor.fetchall()[0][0]
 
-
-# There were 13313487 lines of nodes and ways particularly.
-
-# In[70]:
-
-cursor.execute("select count(*) from (select user from nodes group by user Union                select user from ways group by user) as sub;")
+cursor.execute("select count(*) from (select user from nodes group by user Union\
+                select user from ways group by user) as sub;")
 cursor.fetchall()[0][0]
-
-
-# There were 4930 unique users contributing to ways and nodes in the file.
-
-# In[74]:
 
 cursor.execute("select count(*) from way_tags where key LIKE 'highway';")
 cursor.fetchall()[0][0]
-
-
-# And, just as a fun fact there are 253,981 entries about highways.
-
-# # Conclusion #
-
-# As can be seen above, there is a lot of information in the New York OSM XML file realeased by OpenStreetMaps.org. There are several formatting issues in just street names. Some of the output also, definitely needs further inquiry though by OpenStreetMaps moderators, for example there are street names in Spanish, and some that even include full addresses which does not exactly meet the convention of the rest of the output: "340 Convent Ave, New York, NY 10031, Estados Unidos".
-# 
-# There is a lot more cleaning to be done in this dataset but also a lot that can be gained from analyzing it as well. Just a small sample can be seen above as user activity can be visualized, and perhaps with further details of users one can determine where most of the information is coming from, and relegate ads to places that have less users contributing
